c4b2046c85a013204eaf9609a63812a406ef80eb
denyys <denyysitbelikov@gmail.com>
2023-04-16 11:48:37 +0300

    home-about full done

--- src/js/burger.js
+++ src/js/burger.js
@@ -3,6 +3,7 @@ function openMobNav(){
     const mobileNav = document.querySelector('.header__nav');
     const body = document.querySelector('body');
     burger.addEventListener('click',() =>{
+        console.log('open')
         mobileNav.classList.toggle('active')
         burger.classList.toggle('active')
         body.classList.toggle('_lock')
