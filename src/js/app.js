732130fc6e2d0fd34bba19a00d95a5788541614d
denyys <denyysitbelikov@gmail.com>
2023-04-17 22:45:41 +0300

    home-course done 90%

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
 import * as flsFunctions from "./modules/functions.js";
-
+import AOS from 'aos';
 flsFunctions.isWebp();
+AOS.init();
 
 import openMobileNav from './burger.js'
 import CountNumbers from './countNumbers.js'
+import courseDataQuery from './CourseQuery.js'
 
 
