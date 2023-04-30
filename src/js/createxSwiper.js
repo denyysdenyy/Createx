12d7a2a8cc4475e4862fc12f3c817e06a081e2c3
denyys <denyysitbelikov@gmail.com>
2023-04-30 23:46:21 +0300

    comments full done

--- src/js/createxSwiper.js
+++ src/js/createxSwiper.js
@@ -1,43 +1,70 @@
-import Swiper, { Navigation,Autoplay} from 'swiper';
+import Swiper, { Navigation,Autoplay,Pagination} from 'swiper';
+
+
 
 function createxSwiper(){
 
-    const homeTeamSeciton = document.querySelector('.home-team')
-    if(homeTeamSeciton){
-        const swiper = new Swiper('.home-team__swiper', {
-
-            modules:[Navigation,Autoplay],
-            autoplay:{
-                delay:2000
-            },
-            
-            navigation:{
-                nextEl:'.home-team-swiper-button-next',
-                prevEl:'.home-team-swiper-button-prev',
-            },
-              breakpoints:{
-                320:{
-                    centeredSlidesBounds:true,
-                    slidesPerView:1,
-                    centeredSlides:true,
-                    spaceBetween:10,
+    function homeTeamSwiper(){
+        const homeTeamSeciton = document.querySelector('.home-team')
+        if(homeTeamSeciton){
+            const swiper = new Swiper('.home-team__swiper', {
+    
+                modules:[Navigation,Autoplay],
+                autoplay:{
+                    delay:2000
                 },
-                525:{
-                    slidesPerView:2,
+                
+                navigation:{
+                    nextEl:'.home-team-swiper-button-next',
+                    prevEl:'.home-team-swiper-button-prev',
                 },
-                991:{
-                    spaceBetween:30,
-                    slidesPerView:3,
-
-                },
-                1200:{
-                    slidesPerView:3,
-                    spaceBetween:30,
-
-                },
-              }
-          });
+                  breakpoints:{
+                    320:{
+                        centeredSlidesBounds:true,
+                        slidesPerView:1,
+                        centeredSlides:true,
+                        spaceBetween:10,
+                    },
+                    525:{
+                        slidesPerView:2,
+                    },
+                    991:{
+                        spaceBetween:30,
+                        slidesPerView:3,
+    
+                    },
+                    1200:{
+                        slidesPerView:4,
+                        spaceBetween:30,
+    
+                    },
+                  }
+              });
+        }
     }
+    homeTeamSwiper()
+
+
+    function commentsSwiper(){
+        const commentsSection = document.querySelector('.comments')
+        if(commentsSection){
+            const swiper = new Swiper('.comments__swiper', {
+                modules:[Navigation,Autoplay,Pagination],
+                loop:true,
+                // autoplay:{
+                //     delay:2000
+                // },
+                speed: 1000,
+                pagination: {
+                    el: '.swiper-pagination',
+                  },
+                centeredSlides:true,
+                centeredSlidesBounds:true,
+
+              });
+        }
+    }
+    commentsSwiper()
 }
 
 createxSwiper()
